12e49ef1d0817a81deb6e00e623d62b196b22206
Polina Zolotukhina <polinalinana030@gmail.com>
2023-11-04 19:30:13 +0300

    Изменены функции сбора информации о зданиях и координат

--- main.py
+++ main.py
@@ -11,27 +11,55 @@ url_buildings_info = 'https://аис.фрт.рф/myhouse'
 url_addresses = 'https://krasnodar.ginfo.ru/ulicy/'
 url_districts_v1 = 'https://krasnodar.kitabi.ru/map/street'
 url_districts_v2 = "https://youkarta.ru/krasnodarskij-kraj/krasnodar-23/"
-url_coordinates_v1 = "https://dadata.ru/product/geocode/"
-url_coordinates_v2 = 'https://docs.mapbox.com/playground/geocoding/'
+url_coordinates = 'https://docs.mapbox.com/playground/geocoding/'
 
 with open(r'files/addresses_v2.json', 'r') as openfile:
     j_object = json.load(openfile)
 
+with open(r'files/buildings_info.json', 'r') as openfile:
+    b_object = json.load(openfile)
+
 try:
     # pass
     # addresses = get_addresses(url_addresses)
-    # buildings_info = get_buildings_info(j_object, url_buildings_info)
+    buildings_info = get_buildings_info(j_object, url_buildings_info)
     # districts = get_districts_v1(url_districts)
-    coordinates = get_coordinates(j_object, url_coordinates_v2)
+    # coordinates = get_coordinates(j_object, url_coordinates)
 except Exception as e:
     print(e)
 
-"""s = 0
+"""unic_num = []
 for k, v in j_object.items():
-    print(k, ":", v)
-    s += len(v)
+    # print(f'-----------------------------{k}-----------------------------')
+    for s, n in v.items():
+        for i in n:
+            if i not in unic_num:
+                unic_num.append(i)
 
-print(s)"""
+print()
+
+unic_num = {}
+unic_key = []
+
+not_null = 0
+null = 0
+for k, v in b_object.items():
+    print()
+    print(f'-----------------------------{k}-----------------------------')
+    print()
+    for s, n in v.items():
+        if n:
+            for i, j in n.items():
+                if i not in unic_num:
+                    unic_num[i] = s
+                # print(f'{s} {i}:   {j}')
+            not_null += 1
+        else:
+            null += 1
+print(not_null, null)
+for i, j in sorted(unic_num.items(), key=lambda x: (len(x[0]))):
+    if '(' in i:
+        print(f'{j} - {i}')"""
+
+
 print("--- %s seconds ---" % ((time.time() - start_time)/60))
-
-#233 #289
